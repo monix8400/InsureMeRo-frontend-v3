e2250a78455c352273adbed3f00ec5624e6cc7f6
Șoaita M. Monica <61684336+monix8400@users.noreply.github.com>
2022-05-11 18:50:23 +0300

    am schimbat culori la chart si facut curat

--- src/app/components/chart/chart.component.ts
+++ src/app/components/chart/chart.component.ts
@@ -1,6 +1,6 @@
 import {Component, ViewChild} from '@angular/core';
-import { ChartConfiguration, ChartEvent, ChartType } from 'chart.js';
-import { BaseChartDirective } from 'ng2-charts';
+import {ChartConfiguration, ChartEvent, ChartType} from 'chart.js';
+import {BaseChartDirective} from 'ng2-charts';
 
 
 @Component({
@@ -8,50 +8,63 @@ import { BaseChartDirective } from 'ng2-charts';
   templateUrl: './chart.component.html',
   styleUrls: ['./chart.component.css']
 })
-export class ChartComponent{
+export class ChartComponent {
 
 
   public lineChartData: ChartConfiguration['data'] = {
     datasets: [
       {
-        data: [ 65, 59, 80, 81, 56, 55, 40 ],
+        data: [65, 59, 80, 81, 56, 55, 40],
         label: 'Series A',
         backgroundColor: 'rgba(123,31,162,0.2)',
         borderColor: 'rgba(123,31,162,1)',
-        pointBackgroundColor: 'rgba(123,31,162,1)',//'rgba(148,159,177,1)',
+        pointBackgroundColor: 'rgba(123,31,162,1)',
         pointBorderColor: '#fff',
         pointHoverBackgroundColor: '#fff',
         pointHoverBorderColor: 'rgba(123,31,162,0.8)',
         fill: 'origin',
       },
       {
-        data: [ 28, 48, 40, 19, 86, 27, 90 ],
+        data: [28, 48, 40, 19, 86, 27, 90],
         label: 'Series B',
         backgroundColor: 'rgba(105,240,174,0.2)',
         borderColor: 'rgba(105,240,174,1)',
-        pointBackgroundColor: 'rgba(105,240,174,1)',//'rgba(77,83,96,1)',
+        pointBackgroundColor: 'rgba(105,240,174,1)',
         pointBorderColor: '#fff',
         pointHoverBackgroundColor: '#fff',
         pointHoverBorderColor: 'rgba(105,240,174,1)',
         fill: 'origin',
       },
     ],
-    labels: [ 'January', 'February', 'March', 'April', 'May', 'June', 'July' ]
+    labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July']
   };
 
   public lineChartOptions: any = {
     elements: {
       line: {
-        tension: 0
+        tension: 0.5
+      }
+    },
+    plugins: {
+      legend: {
+        labels: {
+          color: 'white',
+        }
       }
     },
     scales: {
-      // We use this empty structure as a placeholder for dynamic theming.
-      x: {},
-      'y-axis-1': {
+      x: {
+        grid: {
+          color: '#7d7d7d',
+        },
+        ticks: {
+          color: 'white'
+        }
+      },
+      y: {
         position: 'left',
         grid: {
-          color: '#fff',//'rgba(148,159,177,1)',
+          color: '#7d7d7d',
         },
         ticks: {
           color: 'white'
@@ -59,29 +72,6 @@ export class ChartComponent{
       }
     },
 
-    // plugins: {
-    //   legend: { display: true },
-    //   annotation: {
-    //     annotations: [
-    //       {
-    //         type: 'line',
-    //         scaleID: 'x',
-    //         value: 'March',
-    //         borderColor: 'purple',
-    //         borderWidth: 2,
-    //         label: {
-    //           position: 'center',
-    //           enabled: true,
-    //           color: 'purple',
-    //           content: 'LineAnno',
-    //           font: {
-    //             weight: 'bold'
-    //           }
-    //         }
-    //       },
-    //     ],
-    //   }
-    // }
   };
 
   public lineChartType: ChartType = 'line';
@@ -89,11 +79,11 @@ export class ChartComponent{
   @ViewChild(BaseChartDirective) chart?: BaseChartDirective;
 
   // events
-  public chartClicked({ event, active }: { event?: ChartEvent, active?: {}[] }): void {
+  public chartClicked({event, active}: { event?: ChartEvent, active?: {}[] }): void {
     console.log(event, active);
   }
 
-  public chartHovered({ event, active }: { event?: ChartEvent, active?: {}[] }): void {
+  public chartHovered({event, active}: { event?: ChartEvent, active?: {}[] }): void {
     console.log(event, active);
   }
 }
